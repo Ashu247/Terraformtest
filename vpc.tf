39f4d188e0f753472e50a45831e9de9e754e0391
Ashu247 <54639390+Ashu247@users.noreply.github.com>
2019-09-10 00:13:41 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,8 +8,8 @@ resource "aws_vpc" "default" {
   }
 }
 # Define private subnet
-resource "aws_subnet" "default" {
-vpc_id = "${var.aws_vpc.default.id}"
+resource "aws_subnet" "public-subnet" {
+vpc_id = "${aws_vpc.default.id}"
 cidr_block = "${var.public_subnet_cidr}"
   
 tags {
